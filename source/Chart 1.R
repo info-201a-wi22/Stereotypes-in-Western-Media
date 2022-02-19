fe96af2e8adf9250ca6f9d45cd30204cdc9ffc66
heranmengisteab <heranm@uw.edu>
2022-02-18 17:34:10 -0800

    Update Chart 1.R

--- source/Chart 1.R
+++ source/Chart 1.R
@@ -2,7 +2,6 @@ library("tidyverse")
 library("ggplot2")
 library("dplyr")
 
-rm(list = ls())
 
 race_of_characters <- read.csv("https://raw.githubusercontent.com/info-201a-wi22/Stereotypes-in-Western-Media/main/data/race_of_characters.csv")
 View(race_of_characters)
@@ -10,12 +9,12 @@ View(race_of_characters)
 roc_data <- race_of_characters[32:35,]
 View(roc_data)
 
-demographic <- roc_data$�..demographic
+demographic <- roc_data$ï..demographic
 value <- roc_data$very_important
 
-most_importantance <- roc_data[, c("�..demographic", "very_important")]
+most_importantance <- roc_data[, c("ï..demographic", "very_important")]
 #View(total_importantance)
 
 
 ggplot(data = most_importantance) +
-  geom_col(mapping = aes(x = demographic, y = value, fill = imporantance, position= "fill",))
+  geom_col(mapping = aes(x = demographic, y = value, fill = imporantance))
